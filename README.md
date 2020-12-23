20e6142b64dcf935fc17c3c0d7cac0b6cf47e16a
Miel Vander Sande <miel.vandersande@meemoo.be>
2020-12-23 17:25:55 +0100

    added example notebook for borndigi

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # great_expectations_meemoo
+
+``` batch
+# install the meemoo_util package locally
+cd meemoo_util; pip install -e; cd ..
+
+# run jupyter notebook and open ./great_expectations/notebooks/sql/validation_playground.ipynb
+jupyter notebook
+```
---
+++ meemoo_util/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='meemoo_util',
+      version='0.1',
+      description='Utility functions for meemoo metadata',
+      url='https://github.com/viaacode/meemoo_expectations',
+      author='Miel Vander Sande',
+      author_email='miel.vandersande@meemoo.be',
+      license='MIT',
+      packages=['meemoo_util'],
+      zip_safe=False)
